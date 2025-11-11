8108e542992df1a93dc48e38a1a08862a3735224
Igor da Silva Dias <artscolha@gmail.com>
2025-11-10 21:00:31 -0300

    planilha-financeira V1.0

---
+++ app/page.js
@@ -0,0 +1,9 @@
+"use client"
+
+import SpreadSheet from './spreadsheet'
+
+export default function Home(){
+  return(
+    <SpreadSheet />
+  )
+}
---
+++ app/spreadsheet.js
@@ -0,0 +1,11 @@
+"use client"
+
+import {useState}from'react'
+
+export default function SpreadSheet(){
+    return(
+        <div>
+            <h1>Financial spreadsheet</h1>
+        </div>
+    )
+}
